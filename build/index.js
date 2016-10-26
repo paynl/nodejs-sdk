905d94ba4140ee313c8d79f2ebc77868af6d1258
Andy Pieters <andy@pay.nl>
2016-10-26 15:27:58 +0200

    removed build folder from github

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var config_1 = require('./config');
-exports.Config = config_1.Config;
-var transaction_1 = require('./transaction');
-exports.Transaction = transaction_1.Transaction;
-var paymentmethods_1 = require('./paymentmethods');
-exports.Paymentmethods = paymentmethods_1.Paymentmethods;
-//# sourceMappingURL=index.js.map
