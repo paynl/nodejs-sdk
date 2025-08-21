524569790d2a62e7f440b2cad6140aa370695dd3
Jeroen <jgroenendijk@enrise.com>
2025-08-21 15:52:12 +0200

    add jest for testing

---
+++ jest.config.ts
@@ -0,0 +1,47 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  // collectCoverageFrom: undefined,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // An array of regexp pattern strings used to skip coverage collection
+  // coveragePathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+  // An object that configures minimum threshold enforcement for coverage results
+  // coverageThreshold: undefined,
+
+  // A preset that is used as a base for Jest's configuration
+  preset: "ts-jest",
+
+  // Automatically reset mock state before every test
+  // resetMocks: false,
+
+  // Automatically restore mock state and implementation before every test
+  // restoreMocks: false,
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: [
+    "**/tests/?(*.)+(test).ts"
+  ],
+};
+
+export default config;
---
+++ tests/example.test.ts
@@ -0,0 +1,5 @@
+describe('placeholder test', () => {
+    it('should work', () => {
+        expect(true).toBe(true);
+    });
+});
