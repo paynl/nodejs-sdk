3d44d3b185bd4e7905389f45c64ab594dee91b43
Jeroen <jgroenendijk@enrise.com>
2025-08-21 16:58:57 +0200

    🔧 jest config updates

--- jest.config.ts
+++ jest.config.ts
@@ -13,34 +13,28 @@ const config: Config = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    'src/connect/**'
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
-
-  // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {global: {lines: 80, statements: 80, branches: 80}},
 
   // A preset that is used as a base for Jest's configuration
   preset: "ts-jest",
 
-  // Automatically reset mock state before every test
-  // resetMocks: false,
-
   // Automatically restore mock state and implementation before every test
-  // restoreMocks: false,
+  restoreMocks: false,
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "**/tests/?(*.)+(test).ts"
+    "**/tests/**/?(*.)+(test).ts"
   ],
 };
 
