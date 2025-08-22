e643f2804941a33fd294c182a9cdd6e9edc2e0b0
Max Hoogenbosch <mhoogenbosch@enrise.com>
2025-08-22 13:52:15 +0200

    update jest and eslint configurations, add ts-node dependency, and change TypeScript target to ES2022
    # Conflicts:
    #	tsconfig.json

--- jest.config.ts
+++ jest.config.ts
@@ -3,39 +3,35 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+import type { Config } from 'jest';
 
 const config: Config = {
-  // Automatically clear mock calls, instances, contexts and results before every test
-  clearMocks: true,
+    // Automatically clear mock calls, instances, contexts and results before every test
+    clearMocks: true,
 
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+    // Indicates whether the coverage information should be collected while executing the test
+    collectCoverage: true,
 
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: [
-    'src/connect/**'
-  ],
+    // An array of glob patterns indicating a set of files for which coverage information should be collected
+    collectCoverageFrom: ['src/**'],
 
-  // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+    // The directory where Jest should output its coverage files
+    coverageDirectory: 'coverage',
 
-  // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+    // Indicates which provider should be used to instrument code for coverage
+    coverageProvider: 'v8',
 
-  // An object that configures minimum threshold enforcement for coverage results
-  coverageThreshold: {global: {lines: 80, statements: 80, branches: 80}},
+    // An object that configures minimum threshold enforcement for coverage results
+    coverageThreshold: { global: { lines: 80, statements: 80, branches: 80 } },
 
-  // A preset that is used as a base for Jest's configuration
-  preset: "ts-jest",
+    // A preset that is used as a base for Jest's configuration
+    preset: 'ts-jest',
 
-  // Automatically restore mock state and implementation before every test
-  restoreMocks: false,
+    // Automatically restore mock state and implementation before every test
+    restoreMocks: false,
 
-  // The glob patterns Jest uses to detect test files
-  testMatch: [
-    "**/tests/**/?(*.)+(test).ts"
-  ],
+    // The glob patterns Jest uses to detect test files
+    testMatch: ['**/tests/**/?(*.)+(test).ts'],
 };
 
 export default config;
