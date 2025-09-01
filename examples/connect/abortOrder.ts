275b65af0ca067c270ef91e06075b6370448c73a
Jeroen <jgroenendijk@enrise.com>
2025-09-01 12:02:39 +0200

    add void and abort endpoints

---
+++ examples/connect/abortOrder.ts
@@ -0,0 +1,9 @@
+import { createPayNLClient } from '../../src';
+
+const payNL = createPayNLClient({ apiToken: 'your-api-token', serviceId: 'SL-1234-5678' });
+
+const orderId = '00000000-1111-2222-3333-000000000000';
+
+const order = await payNL.Orders.abort(orderId);
+
+console.log(order.status.action);
